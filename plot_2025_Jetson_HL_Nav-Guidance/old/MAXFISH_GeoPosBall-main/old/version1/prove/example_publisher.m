acce182642c2423d9ca0e5c05a5f5a2071455663
Angelo <angelo.plum@hotmail.it>
2026-03-13 09:19:56 +0100

    Replace all files with updated version

--- plot_2025_Jetson_HL_Nav-Guidance/old/MAXFISH_GeoPosBall-main/old/version1/prove/example_publisher.m
+++
@@ -1,7 +0,0 @@
-node_1 = ros2node("/node_1")
-chatterPub = ros2publisher(node_1,"/chatter","std_msgs/String");
-msg = ros2message(chatterPub);
-msg.data = 'hello world';
-while 1
-    send(chatterPub,msg);
-end
